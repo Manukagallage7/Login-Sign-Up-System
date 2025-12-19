d585ee9c62e4d2c3ff4580abb3d5c2957cb3772b
Manukagallage7 <manukakavinda1110@gmail.com>
2025-12-19 22:30:08 +0530

    Add user registration functionality with input validation

---
+++ app/signup.php
@@ -0,0 +1,60 @@
+<?php
+
+function validate_input($data){
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+if (isset($_POST['first_name']) &&
+    isset($_POST['last_name']) &&
+    isset($_POST['email']) &&
+    isset($_POST['password']) &&
+    isset($_POST['confirm_password'])) {
+
+        include "../db_conn.php";
+
+    $first_name = validate_input($_POST['first_name']);
+    $last_name = validate_input($_POST['last_name']);
+    $email = validate_input($_POST['email']);
+    $password = validate_input($_POST['password']);
+    $confirm_password = validate_input($_POST['confirm_password']);
+
+
+    if(empty($first_name)) {
+        $errorM = "First Name is required.";
+        header("Location: ../signup.php?error=$errorM");
+    } else if(empty($last_name)) {
+        $errorM = "Last Name is required.";
+        header("Location: ../signup.php?error=$errorM");
+    } else if(empty($email)) {
+        $errorM = "Email is required.";
+        header("Location: ../signup.php?error=$errorM");
+    } else if(empty($password)) {
+        $errorM = "Password is required.";
+        header("Location: ../signup.php?error=$errorM");
+    } else if(empty($confirm_password)) {
+        $errorM = "Confirm Password is required.";
+        header("Location: ../signup.php?error=$errorM");
+    } else if($password !== $confirm_password) {
+        $errorM = "Passwords do not match.";
+        header("Location: ../signup.php?error=$errorM");
+    } else {
+        $sql = "SELECT * FROM users WHERE email=?";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([$email]);
+        if($stmt->rowCount() > 0) {
+            $errorM = "Email is already registered.";
+            header("Location: ../signup.php?error=$errorM");
+        } else{
+            
+        }
+    }
+
+    echo "User $first_name <br> $last_name <br> registered successfully with email $email.";
+} else {
+    header("Location: ../signup.php");
+}
+
+?>
