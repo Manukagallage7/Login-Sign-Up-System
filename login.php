287011da6496e2e70d1bfbe11a3a023c4c564e70
Manukagallage7 <manukakavinda1110@gmail.com>
2025-12-19 22:29:54 +0530

    Add login page with email and password fields

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <form>
+        <label>Email</label><br>
+        <input type="email" name="email" placeholder="Email" required><br>
+        <label>Password</label><br>
+        <input type="password" name="password" placeholder="Password" required><br><br>
+        <button type="submit">Login</button>
+        <a href="signup.php">Sign Up</a>
+    </form>
+</body>
+</html>
